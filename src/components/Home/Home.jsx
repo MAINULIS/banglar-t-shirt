6519d5706a5348f2f07a55bba0467855b558b3d5
Mainul Islam <mainulislam6168@gmail.com>
2023-08-24 08:05:09 +0600

    tshirt added to cart and  handle btn set

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
+import { useState } from 'react';
 
 const Home = () => {
     const tshirts = useLoaderData();
-    console.log(tshirts)
+    const [cart, setCart] = useState([]);
+    const handleAddToCart = tshirt =>{
+        console.log(tshirt)
+    }
     return (
-        <div>
-            <h2>This is home {tshirts.length}</h2>
+        <div className='home-container'>
+           <div className='t-shirt-container'>
+           {
+                tshirts.map(tshirt => <Tshirt
+                key={tshirt._id}
+                tshirt ={tshirt}
+                handleAddToCart ={handleAddToCart}
+                ></Tshirt>)
+            }
+           </div>
+           <div className='cart-container'>
+                <Cart></Cart>
+           </div>
         </div>
     );
 };
